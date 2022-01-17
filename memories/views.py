f9fe506ab4a4a7dcc3dbbe72e82b02dcf5867295
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2022-01-17 08:43:43 +0700

    Implement LocationCreateListViewSet

--- memories/views.py
+++ memories/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins, permissions
+from .serializers import LocationSerializer
+from .models import Location
 
 # Create your views here.
+
+class LocationCreateListViewSet(mixins.CreateModelMixin,
+                                mixins.ListModelMixin,
+                                viewsets.GenericViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return super().get_queryset().filter(users=self.request.user.id)
--- srts/urls.py
+++ srts/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from users.views import FacebookLoginView, Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,7 +26,5 @@ urlpatterns = [
     path('rest-auth/', include('rest_auth.urls')),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    url(r'^rest-auth/facebook/$', FacebookLoginView.as_view(), name='fb_login'),
-    url(r'', Home.as_view(), name='home'),
-
-]
+    path('', include('memories.urls')),
+]   
